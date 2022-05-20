d8618f99fb31e00ce65a38b7fabe1c5d96f18c37
Ayush Walia <ayush.walia@booking.com>
2022-05-20 13:13:06 +0200

    Added consoles for displaying messages

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/app';
+
+console.log("Testing");
+console.log("Testing");
+console.log("Testing");
+console.log("Testing");
+console.log("Testing");
+
 render(
     <BrowserRouter>
         <App/>
